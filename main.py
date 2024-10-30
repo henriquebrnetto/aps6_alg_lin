9144b04ca02fffa786d468415a46c4191de8b098
henriquebrnetto <henriquebrnetto02@gmail.com>
2024-10-30 17:37:51 -0300

    dados

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 from utils import create_totals_df
 
 folder = 'data/rice'
@@ -6,6 +7,7 @@ pattern = r'_(\d{4})\.'
 
 df = create_totals_df(folder, pattern)
 
+x_train, x_test, y_train, y_test = train_test_split(df)
 
 plt.plot(df)
 plt.show()
